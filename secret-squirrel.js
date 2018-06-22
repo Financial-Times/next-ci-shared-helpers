5a7ced567a8cff649d55f3bc039dea88bbd2f4bc
Keran Braich <keran.braich@ft.com>
2018-06-22 11:13:01 +0100

    Add files to allow list  🐿 v2.9.0

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -3,7 +3,9 @@ module.exports = {
 		allow: [
 			'helper-generate-build-state-artifacts',
 			'helper-install-puppeteer-deps',
+			'helper-npm-auth-token',
 			'helper-npm-install-peer-deps',
+			'helper-npm-publish',
 			'helper-npm-update',
 			'helper-setup-heroku-cli',
 			'helper-setup-s3-upload',
